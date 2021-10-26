ce93f9ef721d142513dcd251cbfda05b56b1a764
Adrien PELFRESNE <64414295+dirdr@users.noreply.github.com>
2021-10-26 08:00:24 +0200

    Create p9.c

---
+++ C/p9.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+int p9();
+int main(){
+    int answer = p9();
+    printf("%d", answer);
+    return 0;
+}
+int p9(){
+    int a, b, c;
+    int result = 0;
+    for (a = 1; a <= 1000; a++) {
+        for (b = 1; b < 1000; b++) {
+            for (c = 1; c <= 1000; c++) {
+                result = (a + b + c);
+                if (result == 1000) {
+                    int left = (int)pow(a, 2) + (int)pow(b, 2);
+                    int right = (int)pow(c, 2);
+                    if (left == right) {
+                        return a*b*c;
+                    }
+                }
+            }
+        }
+    }
+    return -1;
+}
